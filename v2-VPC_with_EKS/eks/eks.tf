c28fcb197ac89b91a2f798a3abb5fad6de6bbd91
akandeae <akande.ae@gmail.com>
2023-11-27 22:01:08 -0600

    nodegroup edited

--- v2-VPC_with_EKS/eks/eks.tf
+++ v2-VPC_with_EKS/eks/eks.tf
@@ -119,6 +119,7 @@ resource "aws_iam_instance_profile" "worker" {
 ###############################################################################################################
 resource "aws_eks_cluster" "eks" {
   name = "ed-eks-01"
+  version = "1.24"
   role_arn = aws_iam_role.master.arn
 
   vpc_config {
@@ -136,9 +137,11 @@ resource "aws_eks_cluster" "eks" {
   ]
 
 }
+
+
 #################################################################################################################
 
-resource "aws_eks_node_group" "backend" {
+/*resource "aws_eks_node_group" "backend" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "dev"
   node_role_arn   = aws_iam_role.worker.arn
@@ -147,12 +150,47 @@ resource "aws_eks_node_group" "backend" {
   disk_size = "20"
   instance_types = ["t2.small"]
   remote_access {
-    ec2_ssh_key = "widows-east-2-key"
+    ec2_ssh_key = "widows-east-1-key"
     source_security_group_ids = [var.sg_ids]
   } 
   
   labels =  tomap({env = "dev"})
   
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    #aws_subnet.pub_sub1,
+    #aws_subnet.pub_sub2,
+  ]
+}
+*/
+
+/*resource "aws_eks_node_group" "backend2" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "dev2"
+  node_role_arn   = aws_iam_role.worker.arn
+  subnet_ids = [var.subnet_ids[0],var.subnet_ids[1]]
+  capacity_type = "ON_DEMAND"
+  disk_size = "20"
+  instance_types = ["t2.small"]
+  remote_access {
+    ec2_ssh_key = "widows-east-1-key"
+    source_security_group_ids = [var.sg_ids]
+  }
+
+  labels =  tomap({env = "dev2"})
+
   scaling_config {
     desired_size = 2
     max_size     = 3
@@ -171,5 +209,4 @@ resource "aws_eks_node_group" "backend" {
     #aws_subnet.pub_sub2,
   ]
 }
-
-
+*/
